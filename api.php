c39df88b5b6ff7daf9cdd1ae5a3f4fd9c63265b6
Daneris Mendoza <danerismendoza096@gmail.com>
2024-02-10 10:36:06 +0800

    api optimize

--- api.php
+++ api.php
@@ -17,14 +17,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     while ($completed_processes < $total_processes) {
         for ($i = 0; $i < $total_processes; $i++) {
             if ($remaining_burstTime_arr[$i] > 0) {
-                // Process the current process
+                // Process the current process then mark as complete and set the the 3 array
+                // Process completes within time quantum
                 if ($remaining_burstTime_arr[$i] <= $time_quantum) {
-                    // Process completes within time quantum
                     $current_time += $remaining_burstTime_arr[$i];
-                    $completionTime_arr[$i] = $current_time;
                     $remaining_burstTime_arr[$i] = 0;
-                } else {
-                    // Process still needs more time
+
+                    $completionTime_arr[$i] = $current_time;    
+                    $turnaroundTime_arr[$i] = $current_time - $arrivalTime_arr[$i];
+                    $waitingTime_arr[$i] = $turnaroundTime_arr[$i] - $burstTime_arr[$i];
+                    $completed_processes++;
+                } 
+                // Process still needs more time
+                else {
                     $current_time += $time_quantum;
                     $remaining_burstTime_arr[$i] -= $time_quantum;
                 }
@@ -35,18 +40,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     'start_time' => $current_time - $time_quantum,
                     'end_time' => $current_time
                 );
-
-                // Check if process is completed
-                if ($remaining_burstTime_arr[$i] == 0) {
-                    $completed_processes++;
-                    $turnaroundTime_arr[$i] = $current_time - $arrivalTime_arr[$i];
-                    $waitingTime_arr[$i] = $turnaroundTime_arr[$i] - $burstTime_arr[$i];
-                }
             }
         }
     }
 
-   
+
 
     // Construct a response
     $response = array(
@@ -59,11 +57,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             'completion_times' => $completionTime_arr,
             'turnaround_times' => $turnaroundTime_arr,
             'waiting_times' => $waitingTime_arr,
-            'time_quantum' => $time_quantum, 
+            'time_quantum' => $time_quantum,
             'gantt_chart' => $ganttChart,
-            'completion_time_average' => getAverage($completionTime_arr), 
-            'turnaround_time_average' => getAverage($turnaroundTime_arr), 
-            'waiting_time_average' => getAverage($waitingTime_arr), 
+            'completion_time_average' => getAverage($completionTime_arr),
+            'turnaround_time_average' => getAverage($turnaroundTime_arr),
+            'waiting_time_average' => getAverage($waitingTime_arr),
         )
     );
 
@@ -71,9 +69,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo json_encode($response);
 }
 
-function getAverage($arr){
-    $average =  (array_sum($arr))/(count($arr));
+function getAverage($arr)
+{
+    $average =  (array_sum($arr)) / (count($arr));
     return $average;
 }
-
-?>
